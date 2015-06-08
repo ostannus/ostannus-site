8f8a8566846a222801460cebe465248b8a24f3f1
ostannus <oliver.stannus@gmail.com>
2015-06-08 23:41:13 +1000

    Test scripts

---
+++ var/www/html/index.php
@@ -0,0 +1,3 @@
+
+<?php exec('octave /var/www/scripts/test.m');?> 
+<img src="camo.png" alt="Camo" style="width:256px;height:256px">
---
+++ var/www/scripts/test.m
@@ -0,0 +1,5 @@
+
+fleckImage = repmat(rand([1, 1, 3]),[512, 512, 1]);
+
+imwrite(fleckImage, '/var/www/html/camo.png');
+
