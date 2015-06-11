661b7f8a56dfe0decd1e7b150d188171e02634e9
ostannus <oliver.stannus@gmail.com>
2015-06-12 04:58:25 +1000

    Create first complete draft of demo
    Create first complete draft of demo.

--- var/www/html/camo/index.php
+++ var/www/html/camo/index.php
@@ -1,40 +1,33 @@
- <?php
-// outputs the username that owns the running php/httpd process
-// (on a system with the "whoami" executable in the path)
-echo("test<br>");
-echo exec('whoami');
-echo("test");
-?>
 
 <?php exec('/usr/bin/octave /var/www/scripts/camo/camo.m');?> 
-<div>
-    <br>
-    <br>
-    <h1>Flecktarn Camouflage Generator</h1>
-    <br>
-</div>
-<br>
-<br>
-<div>
-    <br>
-    <br>
-    <h2>Output Image</h2>
-    <br>
-    <img src="/camo/camo.png" alt="Camo" style="width:256px;height:256px">
-</div>
-<div>
-    <br>
-    <br>
-    <h2>Randomised Parameters</h2>
-    <br>
-    Lorem Ipsum
-</div>
-<div>
-    <center>
+<center>
+    <div>
+        <br>
+        <br>
+        <h1>Flecktarn Camouflage Generator</h1>
+        <br>
+        <br>Please allow around 10 seconds for page to load properly.
+        <br>
+    </div>
+    <div>
+        <br>
+        <br>
+        <h2>Output Image</h2>
+        <br>
+        <img src="/camo/camo.png" alt="Camo" style="width:256px;height:256px">
+    </div>
+    <div>
+        <br>
+        <br>
+        <h2>Randomised Parameters</h2>
+        <br>
+        <a href="/camo/camo_parameters.txt">Parameters as a text file</a>
+    </div>
+    <div>
         <br>
         <br>
         <h2>Code (GitHub)</h2>
         <br>
         <script src="https://gist.github.com/ostannus/0cda911eb2105b0dae56.js"></script>
-    </center>
-</div>
+    </div>
+</center>
